4e03d16482de44b69cd7bb35e177bd2a1ba7087e
thetarkus <mike@mikemob.com>
2019-05-02 01:47:07 -0400

    Add unordered list html helper class

---
+++ src/html/unorderedList.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace kaw393939\html;
+use kaw393939\html\link;
+
+
+class unorderedList
+{
+    public static function ul(String $items) {
+        return '<ul>' . $items . '</ul>';
+    }
+
+    public static function li(String $text) {
+        return '<li>' . $text . '</li>';
+    }
+
+    public function fromObjectArray(Array $objects) {
+        $items = '';
+        foreach ($objects as $object) {
+            $items .= unorderedList::li(link::a("display.php?id={$object->id}", $object->tableName));
+        }
+        echo unorderedList::ul($items);
+    }
+}
