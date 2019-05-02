43b17d5651efa6068ab426fc6158296ab9f0b810
nurimantjiptarto <nkt5@njit.edu>
2019-05-01 22:50:44 -0400

    Added table name input

--- src/index.php
+++ src/index.php
@@ -9,10 +9,11 @@ require_once '../vendor/autoload.php';
     <title>CSV Import</title>
 </head>
 <body>
-    <h1>Upload CSV File</h1>
-    <form action="process.php" method="POST" enctype="multipart/form-data">
-        <input type="file" name="upfile" />
-        <input type="submit" value="Upload" />
-    </form>
+<h1>Upload CSV File</h1>
+<form action="process.php" method="POST" enctype="multipart/form-data">
+    <p>Input file name:</p> <input type="text" name="name"/><br/>
+    <input type="file" name="upfile" />
+    <input type="submit" value="Upload" />
+</form>
 </body>
 </html>
