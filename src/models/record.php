83699bad676ce3a36f3f19c504edf978a56664db
nurimantjiptarto <nkt5@njit.edu>
2019-05-02 00:29:26 -0400

    Added to asTableRow method

--- src/models/record.php
+++ src/models/record.php
@@ -7,7 +7,7 @@
  */
 
 namespace kaw393939\models;
-use kaw393939\database\table;
+use kaw393939\html\table;
 
 
 class record
@@ -15,14 +15,20 @@ class record
     public function __construct(Array $data)
     {
         foreach ($data as $key => $value) {
-            $this->{$key} = $value;
+            if (!is_numeric($key))
+                $this->{$key} = $value;
         }
     }
 
-    public static function createTable(String $name, Array $records) {
-        $table = table::create($name, $records[0]);
-        if ($table != null)
-            $table->insert(array_shift($records));
-        return $table;
+
+    public function asTableRow() {
+        $cells = '';
+        $fields = get_object_vars($this);
+
+        foreach ($fields as $field) {
+            $cells .= table::td($field);
+        }
+
+        return table::tr($cells);
     }
 }
